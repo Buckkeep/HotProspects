3fed0e32e53a01650d7ab338c932513b49ae04dc
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-14 14:42:13 +0100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 14/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
