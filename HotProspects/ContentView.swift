2716d62df90b293f351b3b47d0681c4a884b009d
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-14 14:57:34 +0100

    Added an EditButton() for clarity

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
         if selection.isEmpty == false {
             Text("You selected \(selection.formatted())")
         }
+        
+        EditButton()
     }
 }
 
