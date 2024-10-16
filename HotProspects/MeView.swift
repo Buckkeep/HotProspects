f652325f2c0059b84d02b821f1ed631f33252216
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-10-16 11:12:39 +0100

    Building our tab bar

---
+++ HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 16/10/2024.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeView()
+}
